733722bb964a218de81d5d4783368706817a8d47
vijay <vreddy@tavisca.in>
2019-06-28 20:04:19 +0530

    adding nutritioninfo file

---
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/NutritionInformation.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class NutritionInformation
+{
+    public int[] Protein;
+    public int[] Carbs;
+    public int[] Fat;
+    public int[] Calories;
+   
+
+    public NutritionInformation(int[] protein, int[] carbs, int[] fat)
+    {
+        Protein = protein;
+        Carbs = carbs;
+        Fat = fat;
+        Calories = GetCalories();
+    }
+    public int[] GetCalories()
+    {
+        int[] calories = new int[Protein.Length];
+        for (int i = 0; i < Protein.Length; i++)
+            calories[i] = Carbs[i] * 5 + Protein[i] * 5 + Fat[i] * 9;
+        return calories;
+    }
+    public int GetSuitableDietIndexForPerson(string diet)
+    { 
+        int max = 0, min = 0;
+        if (diet.Length == 0)
+            return 0;
+        List<int> listone = new List<int>();
+        List<int> listtwo = new List<int>();
+        for (int j = 0; j < Protein.Length; j++)//initializing listone
+            listone.Add(j);
+        for (int x = 0; x < diet.Length; x++)
+        {
+
+            switch (diet[x])
+            {
+                case 'P':
+                    max = GetMaximum(listone, Protein);
+                    UpdateList(listone, listtwo, max, Protein);
+                    break;
+
+                case 'C':
+                    max = GetMaximum(listone, Carbs);
+                    UpdateList(listone, listtwo, max, Carbs);
+                    break;
+
+                case 'F':
+                    max = GetMaximum(listone, Fat);
+                    UpdateList(listone, listtwo, max, Fat);
+                    break;
+
+                case 'T':
+                    max = GetMaximum(listone, Calories);
+                    UpdateList(listone, listtwo, max, Calories);
+                    break;
+
+                case 'p':
+                    min = GetMinimum(listone, Protein);
+                    UpdateList(listone, listtwo, min, Protein);
+                    break;
+
+                case 'c':
+                    min = GetMinimum(listone, Carbs);
+                    UpdateList(listone, listtwo, min, Carbs);
+                    break;
+
+                case 'f':
+                    min = GetMinimum(listone, Fat);
+                    UpdateList(listone, listtwo, min, Fat);
+                    break;
+
+                case 't':
+                    min = GetMinimum(listone, Calories);
+                    UpdateList(listone, listtwo, min, Calories);
+                    break;
+            }
+            listone = listtwo;
+            listtwo = new List<int>();
+            if (listone.Count == 1)
+                return listone[0];
+        }
+        return 0;
+    }
+        private int GetMaximum(List<int> list, int[] nutritionProperty)
+        {
+            int max = int.MinValue;
+            foreach (int k in list)
+                max = Math.Max(max, nutritionProperty[k]);
+            return max;
+
+        }
+        private int GetMinimum(List<int> list, int[] nutritionProperty)
+        {
+            int min = int.MaxValue;
+            foreach (int k in list)
+                min = Math.Min(min, nutritionProperty[k]);
+            return min;
+
+        }
+    private  void UpdateList(List<int> oldList, List<int> updatedList, int number, int[] property)
+    {
+
+        foreach (int k in oldList)
+            if (number == property[k])
+                updatedList.Add(k);
+    }
+
+}
+
--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -9,22 +9,22 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         static void Main(string[] args)
         {
             Test(
-                new[] { 3, 4 }, 
-                new[] { 2, 8 }, 
-                new[] { 5, 2 }, 
-                new[] { "P", "p", "C", "c", "F", "f", "T", "t" }, 
+                new[] { 3, 4 },
+                new[] { 2, 8 },
+                new[] { 5, 2 },
+                new[] { "P", "p", "C", "c", "F", "f", "T", "t" },
                 new[] { 1, 0, 1, 0, 0, 1, 1, 0 });
             Test(
-                new[] { 3, 4, 1, 5 }, 
-                new[] { 2, 8, 5, 1 }, 
-                new[] { 5, 2, 4, 4 }, 
-                new[] { "tFc", "tF", "Ftc" }, 
+                new[] { 3, 4, 1, 5 },
+                new[] { 2, 8, 5, 1 },
+                new[] { 5, 2, 4, 4 },
+                new[] { "tFc", "tF", "Ftc" },
                 new[] { 3, 2, 0 });
             Test(
-                new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 }, 
-                new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 }, 
-                new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 }, 
-                new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" }, 
+                new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 },
+                new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 },
+                new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 },
+                new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" },
                 new[] { 2, 6, 6, 2, 4, 4, 5, 0, 5, 5, 6, 6, 3, 5, 6 });
             Console.ReadKey(true);
         }
@@ -41,112 +41,22 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-
-            //throw new NotImplementedException();
             int[] result = new int[dietPlans.Length];
-            int[] calories = new int[protein.Length];
-            for (int i = 0; i < protein.Length; i++)
-                calories[i] = carbs[i] * 5 + protein[i] * 5 + fat[i] * 9;
-            List<int> listone;
-            List<int> listtwo;
-
-
-            int max = -1, min = 100000;
-
+            var nutritionInformation = new NutritionInformation(protein, carbs, fat);
+            
+            int max, min;
             for (int i = 0; i < dietPlans.Length; i++)
             {
-                string s = dietPlans[i];
-                if (s.Length == 0)
-                    continue;
-                listone = new List<int>();
-                listtwo = new List<int>();
-                for (int j = 0; j < protein.Length; j++)
-                    listone.Add(j);
-                for (int x = 0; x < s.Length; x++)
-                {
-                    max = -1;
-                    min = 100000;
-                    switch (s[x])
-                    {
-                        case 'P':
-
-                            foreach (int k in listone)
-                                max = Math.Max(max, protein[k]);
-                            foreach (int k in listone)
-                                if (max == protein[k])
-                                    listtwo.Add(k);
-                            break;
-
-                        case 'C':
-
-                            foreach (int k in listone)
-                                max = Math.Max(max, carbs[k]);
-                            foreach (int k in listone)
-                                if (max == carbs[k])
-                                    listtwo.Add(k);
-                            break;
-                        case 'F':
-
-                            foreach (int k in listone)
-                                max = Math.Max(max, fat[k]);
-                            foreach (int k in listone)
-                                if (max == fat[k])
-                                    listtwo.Add(k);
-                            break;
-                        case 'T':
-
-                            foreach (int k in listone)
-                                max = Math.Max(max, calories[k]);
-                            foreach (int k in listone)
-                                if (max == calories[k])
-                                    listtwo.Add(k);
-                            break;
-                        case 'p':
-
-                            foreach (int k in listone)
-                                min = Math.Min(min, protein[k]);
-                            foreach (int k in listone)
-                                if (min == protein[k])
-                                    listtwo.Add(k);
-                            break;
-                        case 'c':
-
-                            foreach (int k in listone)
-                                min = Math.Min(min, carbs[k]);
-                            foreach (int k in listone)
-                                if (min == carbs[k])
-                                    listtwo.Add(k);
-                            break;
-                        case 'f':
-
-                            foreach (int k in listone)
-                                min = Math.Min(min, fat[k]);
-                            foreach (int k in listone)
-                                if (min == fat[k])
-                                    listtwo.Add(k);
-                            break;
-                        case 't':
-
-                            foreach (int k in listone)
-                                min = Math.Min(min, calories[k]);
-                            foreach (int k in listone)
-                                if (min == calories[k])
-                                    listtwo.Add(k);
-                            break;
-                    }
-                    listone = listtwo;
-                    listtwo = new List<int>();
-                    if (listone.Count == 1)
-                        break;
-                }
-
-                result[i] = listone[0];
-
-
+                 string diet=dietPlans[i];
+                 if(diet.Length!=0)
+                    result[i] = nutritionInformation.GetSuitableDietIndexForPerson(diet);
             }
 
-            return result;
+           return result;
 
         }
+        
+       
+        
     }
 }
